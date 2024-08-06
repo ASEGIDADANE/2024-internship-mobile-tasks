b0e0255d9686a5688776b06e4ee94331f8f48a6a
ASEGIDADANE <asegidadane27@gmail.com>
2024-08-06 14:56:34 +0300

    Contracts of Data Sources

---
+++ on_boarding/ecommerce_app/lib/features/product/data/data_sources/remote_data_source.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../../core/constants/constants.dart';
+import '../../../../core/errors/exception.dart';
+import '../models/Product_model.dart';
+
+
+
+abstract class ProductRemoteDataSource {
+  Future<ProductModel> getProductById(String cityName);
+}
+
+class ProductRemoteDataSourceImpl extends ProductRemoteDataSource {
+  final http.Client client;
+  ProductRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<ProductModel> getProductById(String id) async {
+    final response = await client.get(Uri.parse(Urls.getProduct(id)));
+
+    if (response.statusCode == 200) {
+      return ProductModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+}
