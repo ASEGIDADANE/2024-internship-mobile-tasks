a6ee49d01fc15d0b3375c8e2b1ef1a7c0021dc79
ASEGIDADANE <asegidadane27@gmail.com>
2024-08-07 23:13:51 +0300

    task13:Implement Network Info

--- on_boarding/ecommerce_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ on_boarding/ecommerce_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,3 +1,15 @@
+// //
+// //  Generated file. Do not edit.
+// //
+
+// import FlutterMacOS
+// import Foundation
+
+// import shared_preferences_foundation
+
+// func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+//   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+// }
 //
 //  Generated file. Do not edit.
 //
@@ -5,8 +17,12 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_macos
+import connectivity_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
